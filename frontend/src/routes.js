9dd1fff2041f14cfd653d2591f501a87ff8d2de3
kader-mb <47367098+kader-mb@users.noreply.github.com>
2020-04-07 13:50:18 +0100

    UI integration

---
+++ frontend/src/routes.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const page1 = React.lazy(() => import("./views/Base/page1/page1"));
+
+const Dashboard = React.lazy(() => import("./views/Dashboard"));
+
+// https://github.com/ReactTraining/react-router/tree/master/packages/react-router-config
+const routes = [
+  { path: "/", exact: true, name: "Home" },
+  { path: "/dashboard", name: "Dashboard", component: Dashboard },
+  { path: "/page1", exact: true, name: "Page1", component: page1 },
+];
+
+export default routes;
---
+++ frontend/src/views/Base/page1/page1.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+
+class page1 extends Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.toggleFade = this.toggleFade.bind(this);
+    this.state = {
+      collapse: true,
+      fadeIn: true,
+      timeout: 300,
+    };
+  }
+
+  toggle() {
+    this.setState({ collapse: !this.state.collapse });
+  }
+
+  toggleFade() {
+    this.setState((prevState) => {
+      return { fadeIn: !prevState };
+    });
+  }
+
+  render() {
+    return <div className="animated fadeIn"></div>;
+  }
+}
+
+export default page1;
